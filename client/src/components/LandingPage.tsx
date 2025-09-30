ac71127b838fb79e8d3c7039bc64802fae071645
prabash19 <prabashdangi19@gmail.com>
2025-09-30 23:45:01 +1300

    Landing page UI created

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -18,14 +18,14 @@ export default function LandingPage() {
   return (
     <div
       className="
-      w-full h-screen bg-gradient-to-br from-indigo-600 via-blue-500 to-purple-500 text-white
+      w-full h-screen bg-gradient-to-br from-purple-600 via-blue-500 to-purple-500 text-white
       flex items-center justify-center flex-col
       "
     >
       <h1 className="font-bold mb-0.5 text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-gray-900 dark:text-white leading-tight">
         Turn Ideas Into Apps
       </h1>
-      <h2 className="font-bold mb-8 text-xl sm:text-xl md:text-xl lg:text-xl text-gray-900 dark:text-gray-200 leading-tight">
+      <h2 className="font-bold mb-8 text-xl  text-gray-300 leading-tight">
         Build any app in seconds — just describe what you want to create{" "}
       </h2>{" "}
       <div className="relative w-1/2">
@@ -33,8 +33,9 @@ export default function LandingPage() {
           autoFocus
           value={textValue}
           onKeyDown={handleKeyDown}
+          placeholder="Build Apps by chatting with AI."
           onChange={(e) => setTextValue(e.target.value)}
-          className="mt-0.5 w-full h-36 p-4 pb-14 text-2xl focus:outline-none resize-none focus:border-black rounded-2xl border-2 border-gray-300 shadow-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
+          className="mt-0.5 w-full h-36 p-4 pb-14 text-2xl border-black outline-none focus:outline-none resize-none focus:border-black rounded-2xl border-2 shadow-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
         ></textarea>
 
         <button
